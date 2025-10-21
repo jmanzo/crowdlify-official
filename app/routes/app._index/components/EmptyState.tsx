64055d0fbe0c5e5da895740057e867a954605eba
Jean Manzo <jdevmanzo@gmail.com>
2025-10-21 13:10:26 -0400

    feat: add EmptyState component for project creation with navigation and instructional content

---
+++ app/routes/app._index/components/EmptyState.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router";
+
+export function EmptyState() {
+  const navigate = useNavigate();
+  
+  return (
+    <s-grid gap="base" justifyItems="center" paddingBlock="large-400">
+        <s-box maxInlineSize="200px" maxBlockSize="200px">
+            <s-image
+                aspectRatio="1/0.5"
+                src="https://cdn.shopify.com/static/images/polaris/patterns/callout.png"
+                alt="A stylized graphic of four characters, each holding a puzzle piece"
+            />
+        </s-box>
+        <s-grid
+            justifyItems="center"
+            maxInlineSize="450px"
+            gap="base"
+        >
+            <s-stack alignItems="center">
+                <s-heading>Start creating projects</s-heading>
+                <s-paragraph>
+                    Create and manage your projects and upload your Kickstarter and/or Indiegogo csv files.
+                </s-paragraph>
+            </s-stack>
+            <s-button-group>
+                <s-button slot="secondary-actions" aria-label="Learn more about creating puzzles"> Learn more </s-button>
+                <s-button 
+                    slot="primary-action" 
+                    aria-label="Add a new project"
+                    onClick={() => navigate('/app/projects/new')}
+                > Create project </s-button>
+            </s-button-group>
+        </s-grid>
+    </s-grid>
+  );
+}
+
